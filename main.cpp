cccac79c59a241f630fe3517e1ecedf77d97f54b
hrshishym <chappy.dogheart@gmail.com>
2016-09-25 13:32:15 +0000

    Add more sphere.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@
 #include "camera.h"
 #include "float.h"
 
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray& r, hitable *world) {
   hit_record rec;
   if(world->hit(r, 0.0, FLT_MAX, rec)){
-    return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target-rec.p), world);
   }
   else {
     vec3 unit_direction = unit_vector(r.direction());
@@ -17,15 +26,16 @@ vec3 color(const ray& r, hitable *world) {
 }
 
 int main() {
-  int nx = 200;
-  int ny = 100;
+  int nx = 400;
+  int ny = 200;
   int ns = 100;
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-  hitable *list[2];
+  hitable *list[3];
   list[0] = new sphere(vec3(0, 0, -1), 0.5);
   list[1] = new sphere(vec3(0, -100.5, -1), 100);
-  hitable *world = new hitable_list(list, 2);
+  list[2] = new sphere(vec3(0.6, 0.2, -0.8), 0.2);
+  hitable *world = new hitable_list(list, 3);
 
   camera cam;
 
@@ -47,5 +57,7 @@ int main() {
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
+  delete world;
+  for(int i = 0; i < 3; i++) delete list[i];
 }
 
