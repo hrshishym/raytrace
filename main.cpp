cade32c12bda2cdb7b72996de5b9bc95db9dad8d
hrshishym <chappy.dogheart@gmail.com>
2016-10-14 00:26:29 +0900

    Improve Focus, and many many Spheres.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,33 @@
+#ifdef _WIN32
+#define M_PI 3.1415926535897932384626433832795
+static long long  x=0x1234ABCD330E;
+
+double drand48() /* 0.0ˆÈã1.0–¢–ž */
+{
+    x = x*0x5DEECE66D + 0xB;
+    return (x&0xFFFFFFFFFFFF)*(1.0/281474976710656.0);
+}
+
+long lrand48() /* 0ˆÈã2147483647ˆÈ‰º */
+{
+    x = x*0x5DEECE66D + 0xB;
+    return(long)(x>>17)&0x7FFFFFFF;
+}
+
+long mrand48() /* -2147483648ˆÈã214748367ˆÈ‰º */
+{
+    x = x*0x5DEECE66D + 0xB;
+    return(long)(x>>16)&0xFFFFFFFF;
+}
+
+void srand48(long s)
+{
+    x = s;
+    x = (x<<16)+0x330E;
+}
+#endif
+
+
 #include <iostream>
 #include "sphere.h"
 #include "hitable_list.h"
@@ -30,16 +60,40 @@ int main() {
   std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
   float R = cos(M_PI / 4);
-  hitable *list[5];
-  list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-  list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-  list[2] = new sphere(vec3(1.0, 0.0, -1.0), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.3));
-  list[3] = new sphere(vec3(-1.0, 0.0, -1.0), 0.5, new dielectric(1.5));
-  list[4] = new sphere(vec3(-1.0, 0.0, -1.0), -0.45, new dielectric(1.5));
-  hitable *world = new hitable_list(list, 5);
 
-  // camera  lookfrom,       lookat,         vup
-  camera cam(vec3(-1, 1, 1), vec3(0, 0, -1), vec3(0, 1, 0), 90, float(nx)/float(ny));
+  const int num = 500;
+  hitable *list[num];
+  list[0] = new sphere(vec3(0, -1000, -1), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+  int i = 1;
+  for(int a = -11; a < 11; a++){
+    for(int b = -11; b < 11; b++){
+      float choose_mat = drand48();
+      vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+      if((center - vec3(4, 0.2, 0)).length() > 0.9){
+        if(choose_mat < 0.8){ // diffuse
+          list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(), drand48()*drand48())));
+        }else if(choose_mat < 0.95){  // metal
+          list[i++] = new sphere(center, 0.2, new metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())), 0.5 * (1 + drand48())));
+        }else{  // glass
+          list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+        }
+      }
+    }
+  }
+  list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+  list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+  list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+  hitable *world = new hitable_list(list, i);
+  int tnum = i;
+
+  vec3 lookfrom(8, 2, 5);
+  vec3 lookat(0, 1, 0);
+  float dist_to_focus = 10.0; // (lookfrom - lookat).length();
+  float aperture = 0.1;
+
+  // camera  lookfrom, lookat, vup
+  camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx)/float(ny), aperture, dist_to_focus);
 
   for(int j = ny - 1; j >= 0; j--){
     for(int i = 0; i < nx; i++){
@@ -61,6 +115,6 @@ int main() {
     }
   }
   delete world;
-  for(int i = 0; i < 5; i++) delete list[i];
+  for(int i = 0; i < tnum; i++) delete list[i];
 }
 
